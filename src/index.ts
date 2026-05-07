0bda558b4aafbb6c3d84b97a2a306b17543e2569
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-05-08 01:59:21 +0530

    almost all routes completed

--- src/index.ts
+++ src/index.ts
@@ -55,10 +55,10 @@ app.post("/api/v1/signup", async (req: express.Request, res: express.Response) =
             password: hashedPassword,
             role: "user"
         });
-        return res.status(201).json({ message: "You are signed up." });
+        return res.status(200).json({ message: "You are signed up." });
     } catch (err: any) {
         if (err.code === 11000) {
-            return res.status(400).json({
+            return res.status(403).json({
                 error: "Username or email already exists."
             })
         }
@@ -92,7 +92,7 @@ app.post("/api/v1/signin", async (req: express.Request, res: express.Response) =
     }
 
     if (!foundUser) {
-        return res.status(401).json({ error: 'Invalid email or password.' });
+        return res.status(403).json({ error: 'Invalid email or password.' });
     }
 
     let passwordMatch;
@@ -103,7 +103,7 @@ app.post("/api/v1/signin", async (req: express.Request, res: express.Response) =
     }
 
     if (!passwordMatch) {
-        return res.status(401).json({ error: 'Invalid email or password.' });
+        return res.status(403).json({ error: 'Invalid email or password.' });
     }
 
     const token = jwt.sign({
@@ -120,8 +120,165 @@ app.post("/api/v1/signin", async (req: express.Request, res: express.Response) =
 
 app.use(userAuthMiddleware);
 
+const contentTypes = ["image", "audio", "video", "article", "tweet", "document", "link", "other"] as const;
 
+app.post("/api/v1/content", async (req: express.Request, res: express.Response) => {
+    const requiredBody = z.object({
+        link: z.string().url(),
+        type: z.enum(contentTypes),
+        title: z.string(),
+        tags: z.array(z.string().transform((tag) => tag.toLowerCase().trim())).optional(),
+    })
 
+    const result = requiredBody.safeParse(req.body);
+
+    if (!result.success) {
+        return res.status(411).json({
+            error: "Invalid input. " + result.error.format()
+        });
+    }
+
+    let foundTags: any[] = [];
+    let newTags: string[] = [];
+    let insertedTags: any[] = [];
+    try {
+        foundTags = await TagModel.find({
+            title: { $in: result.data.tags || [] }
+        }).lean();
+
+        const foundTagsSet = new Set(foundTags.map(tag => tag.title));
+
+        newTags = (result.data.tags || []).filter((tag) => !foundTagsSet.has(tag));
+
+        if (newTags.length > 0) {
+            insertedTags = await TagModel.insertMany(newTags.map((tag) => ({ title: tag })));
+        }
+    } catch (err) {
+        return res.status(500).json({
+            error: "Something went wrong."
+        });
+    }
+
+    const { link, type, title } = result.data;
+    try {
+        await ContentModel.create({
+            link: link,
+            type: type,
+            title: title,
+            tags: [...foundTags, ...insertedTags].map((tag) => tag._id),
+            userId: req.user._id
+        })
+
+        return res.status(201).json({
+            message: "Content created."
+        });
+    } catch (err) {
+        return res.status(500).json({
+            error: "Something went wrong."
+        });
+    }
+});
+
+app.get("/api/v1/contents", async (req: express.Request, res: express.Response) => {
+    const userId = req.user._id;
+
+    try {
+        const contents = await ContentModel.find({ userId: userId }).populate("tags").sort({ createdAt: -1 }).lean();
+        return res.status(200).json({
+            contents: contents
+        });
+    } catch (err) {
+        return res.status(500).json({
+            error: "Something went wrong."
+        });
+    }
+});
+
+app.get("/api/v1/content/title", async (req: express.Request, res: express.Response) => {
+    const title: string = req.query.title as string;
+    const userId = req.user._id;
+
+    if (!title) {
+        return res.status(411).json({
+            error: "Title query parameter is required."
+        });
+    }
+
+    try {
+        const content = await ContentModel.find({ title: title, userId: userId }).populate("tags").sort({ createdAt: -1 }).lean();
+
+        return res.status(200).json({
+            contents: content
+        });
+    } catch (err) {
+        return res.status(500).json({
+            error: "Something went wrong."
+        });
+    }
+});
+
+app.get("/api/v1/content/:id", async (req: express.Request, res: express.Response) => {
+    const contentId = req.params.id as string;
+    const userId = req.user._id;
+
+    if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        return res.status(400).json({
+            error: "Invalid content ID."
+        });
+    }
+
+    try {
+        const content = await ContentModel.findOne({ _id: contentId, userId: userId }).populate("tags").lean();
+        if (!content) {
+            return res.status(404).json({
+                error: "Content not found."
+            });
+        }
+        return res.status(200).json({
+            content: content
+        });
+    } catch (err) {
+        return res.status(500).json({
+            error: "Something went wrong."
+        });
+    }
+});
+
+app.delete("/api/v1/content/:id", async (req: express.Request, res: express.Response) => {
+    const contentId = req.params.id as string;
+    const userId = req.user._id;
+
+    if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        return res.status(400).json({
+            error: "Invalid content ID."
+        });
+    }
+
+    try {
+        const deleted = await ContentModel.findOneAndDelete({
+            _id: contentId,
+            userId: userId
+        });
+
+        if (!deleted) {
+            return res.status(404).json({
+                error: "Content not found."
+            });
+        }
+
+        return res.status(200).json({
+            message: "Content deleted."
+        });
+    } catch (err) {
+        return res.status(500).json({
+            error: "Something went wrong."
+        });
+    }
+});
+
+app.post("/api/v1/brain/share", async (req: express.Request, res: express.Response) => { });
+
+app.post("/api/v1/brain/:sharelink", async (req: express.Request, res: express.Response) => { });
 
 
 startServer();
