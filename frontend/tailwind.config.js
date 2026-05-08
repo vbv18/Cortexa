057a7644573191023c1bf79230e7efa0768589c3
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-05-09 01:52:07 +0530

    updated custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,16 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                primaryBtnBg: "#5046e4",
+                primaryBg: "#f9fafb",
 
+                secondaryBtnBg: "#e0e7ff",
+                secondaryBtnTxt: "#625ad4",
+
+                tagBg: "#eef2ff",
+                tagTxt: "#817cec",
+            }
         },
     },
 
