29c59e6b957aae9bba267dd3563ee2c5383b21fb
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-05-09 14:56:35 +0530

    updated icons

---
+++ frontend/src/components/icons/YoutubeIcon.tsx
@@ -0,0 +1,29 @@
+const sizeStyles: { [key: string]: string } = {
+    sm: "w-4 h-4",
+    md: "w-5 h-5",
+    lg: "w-7 h-7",
+};
+
+interface YoutubeIconProps {
+    size?: "sm" | "md" | "lg";
+}
+
+export const YoutubeIcon = (props: YoutubeIconProps) => {
+    const sizeClass = sizeStyles[props.size || "md"];
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className={sizeClass}
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M23.5 6.5s-.2-1.6-.8-2.3c-.8-.8-1.7-.8-2.1-.9C17.4 3 12 3 12 3s-5.4 0-8.6.3c-.5.1-1.4.1-2.1.9C.7 4.9.5 6.5.5 6.5S0 8.4 0 10.3v1.4c0 1.9.5 3.8.5 3.8s.2 1.6.8 2.3c.8.8 1.8.8 2.3.9 1.7.2 7.3.3 7.3.3s5.4 0 8.6-.3c.5-.1 1.4-.1 2.1-.9.6-.7.8-2.3.8-2.3s.5-1.9.5-3.8v-1.4c0-1.9-.5-3.8-.5-3.8zM9.75 15.02V8.98l6.5 3.02-6.5 3.02z"
+            />
+        </svg>
+    )
+}
