c4d0f7b745dedf7a608856fb2a1f330dff09b79d
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-05-09 17:20:35 +0530

    tested button and card components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,69 @@
 import './App.css'
 import { Button } from "./components/Button"
-import { PlusIcon } from './icons/PlusIcon'
-import { ShareIcon } from './icons/ShareIcon'
+import { Card } from './components/Card'
+import { PlusIcon } from './components/icons/PlusIcon'
+import { ShareIcon } from './components/icons/ShareIcon'
 
 export default function App() {
-  return (
-    <div className="p-2 m-2 flex gap-4">
-      <Button
-        variant="secondary"
-        size="md"
-        text="Share Brain"
-        startIcon={<ShareIcon />}
-        disabled={false}
-        onClick={() => alert('Button Clicked!')}
-      />
+	return (
+		<div className='h-screen bg-primary-bg'>
+			<div className="p-2 m-2 flex gap-4">
+				<Button
+					variant="secondary"
+					size="sm"
+					text="Share Brain"
+					startIcon={<ShareIcon />}
+					disabled={false}
+					onClick={() => alert('Button Clicked!')}
+				/>
 
-      <Button
-        variant="primary"
-        size="md"
-        text="Add Content"
-        startIcon={<PlusIcon />}
-        disabled={false}
-        onClick={() => alert('Button Clicked!')}
-      />
-    </div>
-  )
+				<Button
+					variant="primary"
+					size="md"
+					text="Add Content"
+					startIcon={<PlusIcon />}
+					disabled={false}
+					onClick={() => alert('Button Clicked!')}
+				/>
+			</div>
+
+			<div className='flex flex-wrap gap-4'>
+				<Card
+					icon="Youtube"
+					title="Project Details"
+					link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+					tag={[
+						{
+							_id: "1",
+							title: "react"
+						},
+						{
+							_id: "2",
+							title: "typescript"
+						},
+						{
+							_id: "3",
+							title: "frontend"
+						}
+					]}
+				/>
+
+				<Card
+					icon="Twitter"
+					title="Interesting AI Thread"
+					link="https://twitter.com"
+					tag={[
+						{
+							_id: "4",
+							title: "ai"
+						},
+						{
+							_id: "5",
+							title: "ml"
+						}
+					]}
+				/>
+			</div>
+		</div>
+	)
 }
